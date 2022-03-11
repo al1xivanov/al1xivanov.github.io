9725756ee7ab5137946258d4defba0c3a10f10e0
al1xivanov <al1xivanov@yandex.ru>
2022-03-11 10:21:57 +0300

    Adding HB AdFox

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-//const show = document.addEventListener(this.document, ev => {document.querySelector(content)? console.log(): DOMException})
-//some change 1
+const a = () => {windows.Ya.headerBidding.getBidsRecieved()}
+console.trace(async (a()))
