df0076e00c1c3063eafbf45eae8806a2f1033ad8
al1xivanov <al1xivanov@yandex.ru>
2022-03-11 11:46:26 +0300

    Editing bidder part

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const a = window.Ya.headerBidding.getBidsRecieved()
-console.trace(a)
+// const a = window.Ya.headerBidding.getBidsRecieved()
+// console.trace(a)
 //adding stack trace Bids winner stakes
