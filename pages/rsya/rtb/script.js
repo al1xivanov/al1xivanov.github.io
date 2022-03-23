87d80c7d9bc71239e7c7eaf4d75543cfe112dbe7
al1xivanov <al1xivanov@yandex.ru>
2022-03-23 13:05:03 +0300

    adding RTB to site

--- pages/rsya/rtb/script.js
+++ pages/rsya/rtb/script.js
@@ -1,3 +1,335 @@
 // const a = window.Ya.headerBidding.getBidsRecieved()
 // console.trace(a)
 //adding stack trace Bids winner stakes
+
+{
+    "settings"
+:
+    {
+        "12"
+    :
+        {
+            "borderColor"
+        :
+            "DDDCDA", "viewNotices"
+        :
+            [], "horizontalAlign"
+        :
+            true, "adFilter"
+        :
+            "yabs.NjkwODIxNTExMgo3MjA1NzYwMjkyODMxNDE4Mgo3MjA1NzYwMjkzODc4Mzc5OQo3NTE5OTkzMjg0CjcyMDU3NjAyODA5NTU1Mjcz", "limit"
+        :
+            "1", "blockId"
+        :
+            "R-A-428363-12", "linkTail"
+        :
+            "https://an.yandex.ru/rtbcount/1KpJ-4zW0Oq100000000U9nJR4jGDtxFVAF6i70HpwPBuUrYbhNPpN4CGE094mazEd5sRQnt9nj3AYDGFBDmqPt38F5I7Y2lzYgGQ6K4aLCP6GGO16OoCkza08E5Z5O54DPAHdzJ26iZo0G1DY2nbt4P5m_3KR3CLKQGy5r61Xa6Xh-CivWO6EOoWKIMCka0gRLClq0wcSeUQNzUdNHI-YpJVo1uolI2cO7X5PF0XwQqNuMB61XEShy-fx68bU4l4ol8ISmWifTP4UGmPf1XSvdPGKvXPc9GW3Y5kNabxzO-xbcwLy7oTy7aJsO79twm_2mMXOnSnpnVNGiN8uyAoqGJlzImWj30xBImxCGWop23mF2aXInpkmp1YbUmtDNQmCAuWuNf00lpIPR2puKDxtytTrvW-V5v_upfgtR_5xt90Wc13_OFMTB0u9xzMnQGxoUmJfsmeA5XWZS0yX5oKG00", "bannerIds"
+        :
+            ["72057602928314182"], "bgColor"
+        :
+            "FFFFFF", "name"
+        :
+            "posterHorizontal", "intersectionVisibility"
+        :
+            "1"
+        }
+    }
+,
+    "common"
+:
+    {
+        "userRegion"
+    :
+        "RU", "designId"
+    :
+        "2107347397", "commonRtbVisibilityProb"
+    :
+        "100", "detailedDeviceType"
+    :
+        "33", "reloadTimeout"
+    :
+        "30", "stripeType"
+    :
+        "1", "linkHead"
+    :
+        "https://an.yandex.ru/count/WF8ejI_zO2i0HGS0P0jzcv75XS0ZhGK0Am5zQuDU000003YMfJI80Wov0jOgn_7l_nWCy0BiXSFr28PW9pFl0zXyqGPsuYlpgsB96t6YtCFILuSMW0e1Y0gF0QWAw0U82nYg2n0fDZG_SQ80071nwyueel0B2wWFySIoaAlwrSCMu0y1a12JevpZbfVt_vy1-1384OA4pFeFwH9nyt60x_k3YU0K0V0LmOhsxAEFlFnZW1QLaA20W91BoHO00000003mFmm0Nmf3uFGg0R8QQxw13de0-wm481RhTa_aONnhvxWM_jWu0XVD0t15ev00Ybca0m00~1", "commonRtbVisibility"
+    :
+        "1", "deviceType"
+    :
+        "11", "isYandexPage"
+    :
+        "0", "userGroup"
+    :
+        "16", "pageLang"
+    :
+        "1", "syntheticMatch"
+    :
+        "", "stripeAnimation"
+    :
+        "0", "stationaryConnection"
+    :
+        "1", "showAdTune"
+    :
+        "", "isYandex"
+    :
+        "1"
+    }
+,
+    "direct"
+:
+    {
+        "ads"
+    :
+        [{
+            "domain": "eda.yandex",
+            "liked": "0",
+            "orderOptions": "274945150976",
+            "bannerCategories": [],
+            "abuseUrl": "//an.yandex.ru/abuse/082QVPkHnOVpIfG509e0CNrhWru00000E9QbD73GyiD2a06CdSIUme20W0AO0OoTn9v2s07muiuUw046e0BUf-eMW0FjkV7W3uW3qDoJamN91ePW9pFl0zXysGO0000G1W000Aa7SQBSmz9NXnRG1mBG2820W802Y0iOgWiGAJOqFt6Y001mSUlEAA850C0J____________0U0K0G00~1",
+            "geoDistanceRegular": "",
+            "adId": "72057602928314182",
+            "bodyHyph": "",
+            "punyDomain": "eda.yandex",
+            "algId": "12",
+            "blockFavicon": "0",
+            "url": "https://an.yandex.ru/count/WXSejI_zO0i2RH00f1fzcv75T5BBj0K02mDzQuDU000003YMfJJGyiD2W07gmBK2Y06Qq-_9Iv01Z9t4diA0W802c06CdSIUGg01YgW1YhW1yE7aW1_YpXxW0SYcj1de0GOCe0BUf-eMkGBMAiVnx_yO3F02x8N3zGY00-svyU0FY0FGt9EJ1PW3_UG2-nRu18te7OW5q9iLa0MDw1sW1PwC2QW5cSe7i0MPoWUu1PdAwym3u0KCXc0dC-y3s7pH1dRYA_EhOiaRsGO0000G1W000Aa7SQBSmz9NXnR2xvEr6AeB42asD3zneW00S77hpYYYw0kLwmtm2mle31kO3S273Q0Em8GzySIoaAlwrSCMsG_u1JzRcjxpF-0F0O0GY-kb690GawESuvQNz_-V-102WeJC-W_W4PsLnW7e4QdIYQd7gENyFV5TjK0v7jY3ECZTF-aISVDnWE_xWudu4WFuyx-Obwgx_-0JcSe7Y1JKllRvXT6YBQ0KcSe7g1IDwCpJX_G5u1G1w1GOq1NCquVq1TWLmOhsxAEFlFnZy80MbP2WW82GIw0MqEdfymN95j0MuEpblW4T07eM2RficIvQFHnI735jK7POa3MtVcJYDZK8G8McrWTKPONnumQHPqvmMX_vDyO4i7gd2sg3mm5NXCe8cFSvYfV6XowifDdnfXN8I-wqM6XensQ3C5oJF6rYfm2AMKm2~1",
+            "body": "<hl>Доставка</hl> <hl>продуктов</hl> и товаров на дом. Новый взгляд на магазин у дома",
+            "orderTags": [],
+            "secondTitle": "",
+            "bannerFlags": "",
+            "targetUrl": "https://eda.yandex/lavka/?utm_medium=cpc&utm_source=yasearch&utm_campaign=50951563.[EDA]MA_Lavka-goal_RU-MOS-MOW_restype-network_NU&utm_content=8890386245.4161575032&utm_term=доставка продуктов.20287580570",
+            "title": "Яндекс.Лавка &mdash; Продукты за 15 минут",
+            "sitelinks": [{"url": "", "targetUrl": "", "title": ""}, {
+                "url": "",
+                "targetUrl": "",
+                "title": ""
+            }, {"url": "", "targetUrl": "", "title": ""}, {"url": "", "targetUrl": "", "title": ""}],
+            "oid": "32102584",
+            "debug": "",
+            "region": "Москва",
+            "titleHyph": "",
+            "warning": "",
+            "linkTail": "=WO0ejI_zO0W1jGi051CblQol20BGyiD2W07gmBK2Y06Qq-_9Iv01Z9t4diA0W802c06CdSIUGg01YgW1YhW1yE7aW1_O0V3YpXxW0SYcj1de0GQW0jwdwXQ00-svyU0FY0FGt9EJ1PW3_UG2i0C2-nQ81T2R5P05ZUWTi0MPoWUu1PdA1-lC0-053AW6gWFW1uOAyGS00CBlaxNP2t6YtCFILuSMw0kLwmte31kO3S273J-W3i24FTaF-0K_MvhUyp-048lhfHZW4PsLnW7e4QdIYQd7gENyFV5TjK0v7jY3ECZTF-aISVDnWE_xWucO4mQe4_ZplvYNghl_5U0JcSe7Y1JKllRvXT6YBQ0KcSe7g1IDwCpJX_G5w1GOq1NCquVq1TWLmOhsxAEFlFnZe1RGwUdp1SaMq1RWxEM-0HK06ndYOToi7i8Qo21DXp0rUzO8qHSLiGeQQq2e2H0SS8L68CTmn2XEpr782P-iW9mORwW45gTpMim6x71Dye9jeWW0~1",
+            "abuseInfo": "https://an.yandex.ru/count/WWSejI_zOFa1xGy0H1bzcv75fHZ2mmK0-G9zQuDU000003YMfJJGyiD2W07gmBK2Y06Qq-_9Iv01Z9t4diA0W802c06CdSIUGg01YgW1YhW1yE7aW1_00LZO0V3YpXxW0SYcj1de0GOCe0BUf-eMkGBMAiVnx_yO3F02x8N3zGY00-svyU0FY0FGt9EJ1PW3_UG2-nQ81T2R5P05ZUWTi0MPoWUu1PdAwym3u0KCXc0dC-y3s7pH1dRYA_EhOiaRSQBSmz9NXnR2xvEr6AeB42asD3zneW00S77hpYYYw0kLwmtm2mle31kO3S273J-W3i24FV74if2h-jN35jaF-0K_MvhUyp_W3m6048lhfHYG49EZdEEMb_V_d_WG0eA4pFeFu16TbSO1w16fqecfnwZb_3tnNRL0EHxOWpZ8tJ_f4d7pSO3l-uE91FZplvYNghl_u1EPoWU85DI-zlc5qQ8je1IPoWUe58tepDE7z0NW507e51ZG5SpJX_G5s1N1YlRieu-y_6FmW1QLaA20W91Be1RGwUdp1SaMq1RWxEM-0I00UXO9kcoP9Lez_52S28Zmmae06iYQ1pOoSPiQ1A0YKki3cBB2-5-3oACcM45ZGq9j8PLZOqm0cNUwW3Ku7705feo0wdCK7AwBvi8L7neUfDcvkmeikjnfiJ08pZK9qIOs3Q2OHJe0~1",
+            "path": "",
+            "geoDistance": "",
+            "age": "",
+            "pathUrl": "",
+            "images": [["//avatars.mds.yandex.net/get-direct/2799532/DlSUtd-uBOvM08AY30017g/x90", "90", "90", {
+                "ColorWizButton": "#C69867",
+                "ColorWizButtonText": "#461300",
+                "ColorWizBackRight": "#E6BC80",
+                "smart-center": {"y": 0, "h": 26, "x": 6, "w": 84},
+                "smart-centers": [{"y": 0, "h": 26, "x": 6, "w": 84}, {"y": 0, "h": 51, "x": 1, "w": 90}, {
+                    "y": 0,
+                    "h": 82,
+                    "x": 8,
+                    "w": 82
+                }, {"y": 0, "h": 28, "x": 11, "w": 80}, {"y": 0, "h": 87, "x": 20, "w": 66}, {
+                    "y": 0,
+                    "h": 68,
+                    "x": 0,
+                    "w": 90
+                }],
+                "ColorWizBack": "#DDB079"
+            }], ["//avatars.mds.yandex.net/get-direct/2799532/DlSUtd-uBOvM08AY30017g/y90", "90", "90", {
+                "ColorWizButton": "#C69867",
+                "ColorWizButtonText": "#461300",
+                "ColorWizBackRight": "#E6BC80",
+                "smart-center": {"y": 0, "h": 26, "x": 6, "w": 84},
+                "smart-centers": [{"y": 0, "h": 26, "x": 6, "w": 84}, {"y": 0, "h": 51, "x": 1, "w": 90}, {
+                    "y": 0,
+                    "h": 82,
+                    "x": 8,
+                    "w": 82
+                }, {"y": 0, "h": 28, "x": 11, "w": 80}, {"y": 0, "h": 87, "x": 20, "w": 66}, {
+                    "y": 0,
+                    "h": 68,
+                    "x": 0,
+                    "w": 90
+                }],
+                "ColorWizBack": "#DDB079"
+            }], ["//avatars.mds.yandex.net/get-direct/2799532/DlSUtd-uBOvM08AY30017g/x80", "80", "80", {
+                "ColorWizButton": "#C69867",
+                "ColorWizButtonText": "#461300",
+                "ColorWizBackRight": "#E6BC80",
+                "smart-center": {"y": 0, "h": 23, "x": 5, "w": 75},
+                "smart-centers": [{"y": 0, "h": 23, "x": 5, "w": 75}, {"y": 0, "h": 45, "x": 1, "w": 80}, {
+                    "y": 0,
+                    "h": 73,
+                    "x": 7,
+                    "w": 73
+                }, {"y": 0, "h": 25, "x": 9, "w": 71}, {"y": 0, "h": 78, "x": 18, "w": 58}, {
+                    "y": 0,
+                    "h": 60,
+                    "x": 0,
+                    "w": 80
+                }],
+                "ColorWizBack": "#DDB079"
+            }], ["//avatars.mds.yandex.net/get-direct/2799532/DlSUtd-uBOvM08AY30017g/y80", "80", "80", {
+                "ColorWizButton": "#C69867",
+                "ColorWizButtonText": "#461300",
+                "ColorWizBackRight": "#E6BC80",
+                "smart-center": {"y": 0, "h": 23, "x": 5, "w": 75},
+                "smart-centers": [{"y": 0, "h": 23, "x": 5, "w": 75}, {"y": 0, "h": 45, "x": 1, "w": 80}, {
+                    "y": 0,
+                    "h": 73,
+                    "x": 7,
+                    "w": 73
+                }, {"y": 0, "h": 25, "x": 9, "w": 71}, {"y": 0, "h": 78, "x": 18, "w": 58}, {
+                    "y": 0,
+                    "h": 60,
+                    "x": 0,
+                    "w": 80
+                }],
+                "ColorWizBack": "#DDB079"
+            }], ["//avatars.mds.yandex.net/get-direct/2799532/DlSUtd-uBOvM08AY30017g/y150", "150", "150", {
+                "ColorWizButton": "#C69867",
+                "ColorWizButtonText": "#461300",
+                "ColorWizBackRight": "#E6BC80",
+                "smart-center": {"y": 0, "h": 44, "x": 10, "w": 140},
+                "smart-centers": [{"y": 0, "h": 44, "x": 10, "w": 140}, {"y": 0, "h": 84, "x": 1, "w": 150}, {
+                    "y": 0,
+                    "h": 136,
+                    "x": 14,
+                    "w": 136
+                }, {"y": 0, "h": 46, "x": 18, "w": 133}, {"y": 0, "h": 146, "x": 33, "w": 109}, {
+                    "y": 0,
+                    "h": 113,
+                    "x": 0,
+                    "w": 150
+                }],
+                "ColorWizBack": "#DDB079"
+            }], ["//avatars.mds.yandex.net/get-direct/2799532/DlSUtd-uBOvM08AY30017g/x160", "160", "160", {
+                "ColorWizButton": "#C69867",
+                "ColorWizButtonText": "#461300",
+                "ColorWizBackRight": "#E6BC80",
+                "smart-center": {"y": 0, "h": 47, "x": 11, "w": 149},
+                "smart-centers": [{"y": 0, "h": 47, "x": 11, "w": 149}, {"y": 0, "h": 90, "x": 1, "w": 160}, {
+                    "y": 0,
+                    "h": 145,
+                    "x": 15,
+                    "w": 145
+                }, {"y": 0, "h": 49, "x": 19, "w": 141}, {"y": 0, "h": 155, "x": 35, "w": 117}, {
+                    "y": 0,
+                    "h": 120,
+                    "x": 0,
+                    "w": 160
+                }],
+                "ColorWizBack": "#DDB079"
+            }], ["//avatars.mds.yandex.net/get-direct/2799532/DlSUtd-uBOvM08AY30017g/y160", "160", "160", {
+                "ColorWizButton": "#C69867",
+                "ColorWizButtonText": "#461300",
+                "ColorWizBackRight": "#E6BC80",
+                "smart-center": {"y": 0, "h": 47, "x": 11, "w": 149},
+                "smart-centers": [{"y": 0, "h": 47, "x": 11, "w": 149}, {"y": 0, "h": 90, "x": 1, "w": 160}, {
+                    "y": 0,
+                    "h": 145,
+                    "x": 15,
+                    "w": 145
+                }, {"y": 0, "h": 49, "x": 19, "w": 141}, {"y": 0, "h": 155, "x": 35, "w": 117}, {
+                    "y": 0,
+                    "h": 120,
+                    "x": 0,
+                    "w": 160
+                }],
+                "ColorWizBack": "#DDB079"
+            }], ["//avatars.mds.yandex.net/get-direct/2799532/DlSUtd-uBOvM08AY30017g/x180", "180", "180", {
+                "ColorWizButton": "#C69867",
+                "ColorWizButtonText": "#461300",
+                "ColorWizBackRight": "#E6BC80",
+                "smart-center": {"y": 0, "h": 53, "x": 12, "w": 168},
+                "smart-centers": [{"y": 0, "h": 53, "x": 12, "w": 168}, {"y": 0, "h": 101, "x": 1, "w": 180}, {
+                    "y": 0,
+                    "h": 164,
+                    "x": 16,
+                    "w": 164
+                }, {"y": 0, "h": 55, "x": 21, "w": 159}, {"y": 0, "h": 175, "x": 40, "w": 131}, {
+                    "y": 0,
+                    "h": 135,
+                    "x": 0,
+                    "w": 180
+                }],
+                "ColorWizBack": "#DDB079"
+            }], ["//avatars.mds.yandex.net/get-direct/2799532/DlSUtd-uBOvM08AY30017g/y180", "180", "180", {
+                "ColorWizButton": "#C69867",
+                "ColorWizButtonText": "#461300",
+                "ColorWizBackRight": "#E6BC80",
+                "smart-center": {"y": 0, "h": 53, "x": 12, "w": 168},
+                "smart-centers": [{"y": 0, "h": 53, "x": 12, "w": 168}, {"y": 0, "h": 101, "x": 1, "w": 180}, {
+                    "y": 0,
+                    "h": 164,
+                    "x": 16,
+                    "w": 164
+                }, {"y": 0, "h": 55, "x": 21, "w": 159}, {"y": 0, "h": 175, "x": 40, "w": 131}, {
+                    "y": 0,
+                    "h": 135,
+                    "x": 0,
+                    "w": 180
+                }],
+                "ColorWizBack": "#DDB079"
+            }], ["//avatars.mds.yandex.net/get-direct/2799532/DlSUtd-uBOvM08AY30017g/y300", "300", "300", {
+                "ColorWizButton": "#C69867",
+                "ColorWizButtonText": "#461300",
+                "ColorWizBackRight": "#E6BC80",
+                "smart-center": {"y": 0, "h": 88, "x": 20, "w": 280},
+                "smart-centers": [{"y": 0, "h": 88, "x": 20, "w": 280}, {"y": 2, "h": 169, "x": 1, "w": 300}, {
+                    "y": 0,
+                    "h": 273,
+                    "x": 27,
+                    "w": 273
+                }, {"y": 0, "h": 92, "x": 35, "w": 265}, {"y": 0, "h": 291, "x": 66, "w": 219}, {
+                    "y": 0,
+                    "h": 225,
+                    "x": 0,
+                    "w": 300
+                }],
+                "ColorWizBack": "#DDB079"
+            }], ["//avatars.mds.yandex.net/get-direct/2799532/DlSUtd-uBOvM08AY30017g/x450", "450", "450", {
+                "ColorWizButton": "#C69867",
+                "ColorWizButtonText": "#461300",
+                "ColorWizBackRight": "#E6BC80",
+                "smart-center": {"y": 0, "h": 132, "x": 30, "w": 420},
+                "smart-centers": [{"y": 0, "h": 132, "x": 30, "w": 420}, {"y": 2, "h": 253, "x": 1, "w": 450}, {
+                    "y": 0,
+                    "h": 409,
+                    "x": 41,
+                    "w": 409
+                }, {"y": 0, "h": 138, "x": 53, "w": 398}, {"y": 0, "h": 437, "x": 100, "w": 328}, {
+                    "y": 0,
+                    "h": 338,
+                    "x": 0,
+                    "w": 450
+                }],
+                "ColorWizBack": "#DDB079"
+            }], ["//avatars.mds.yandex.net/get-direct/2799532/DlSUtd-uBOvM08AY30017g/y450", "450", "450", {
+                "ColorWizButton": "#C69867",
+                "ColorWizButtonText": "#461300",
+                "ColorWizBackRight": "#E6BC80",
+                "smart-center": {"y": 0, "h": 132, "x": 30, "w": 420},
+                "smart-centers": [{"y": 0, "h": 132, "x": 30, "w": 420}, {"y": 2, "h": 253, "x": 1, "w": 450}, {
+                    "y": 0,
+                    "h": 409,
+                    "x": 41,
+                    "w": 409
+                }, {"y": 0, "h": 138, "x": 53, "w": 398}, {"y": 0, "h": 437, "x": 100, "w": 328}, {
+                    "y": 0,
+                    "h": 338,
+                    "x": 0,
+                    "w": 450
+                }],
+                "ColorWizBack": "#DDB079"
+            }]]
+        }], "directTitle"
+    :
+        {
+            "url"
+        :
+            "https://direct.yandex.ru/?partner", "userCurrency"
+        :
+            "&#8381;", "title"
+        :
+            "Яндекс.Директ"
+        }
+    }
+}
